ba3575bcb0b12691b59bcbc798c77c3b50660d57
Sebastian <seblowes@btinternet.com>
2022-07-21 13:36:20 +0100

    (feat): Assign rotated values

--- cube.py
+++ cube.py
@@ -22,25 +22,35 @@ for side in range(len(cube)):
         for column in range(len(cube[side][row])):
             cube[side][row][column] = side_notation[side]
 
-def make_rotation():
 
-    side = str(input("What Side to Rotate?\n "))
-    rotation = str(input("Which Direction?\n "))
+side = str(input("What Side to Rotate?\n "))
+rotation = str(input("Which Direction?\n "))
 
-    side_co = cube_side_select.side_select(side)
+side_co = cube_side_select.side_select(side)
 
-    current_cube_side = np.empty(25, dtype=int).reshape(5,5)
+current_cube_side = np.empty(25, dtype=int).reshape(5,5)
 
-    for row in range(len(current_cube_side)):
-        for column in range(len(current_cube_side[row])):
-            if len(side_co[row][column]) != 0:
-                current_cube_side[row][column] = eval("cube"+side_co[row][column])
-            else:
-                current_cube_side[row][column] = 7
+for row in range(len(current_cube_side)):
+    for column in range(len(current_cube_side[row])):
+        if len(side_co[row][column]) != 0:
+            current_cube_side[row][column] = eval("cube"+side_co[row][column])
+        else:
+            current_cube_side[row][column] = 7
 
-    if rotation == "clockwise":
-        return np.rot90(current_cube_side, k=3)
-    if rotation == "anti-clockwise":
-        return np.rot90(current_cube_side, k=1)
+print(current_cube_side)
 
-    
+if rotation == "clockwise":
+    current_cube_side = np.rot90(current_cube_side, k=3)
+if rotation == "anti-clockwise":
+    current_cube_side = np.rot90(current_cube_side, k=1)
+
+print(current_cube_side)
+
+for row in range(len(current_cube_side)):
+    for column in range(len(current_cube_side[row])):
+        if len(side_co[row][column]) != 0:
+            exec("cube"+side_co[row][column] + "=" + str(current_cube_side[row][column]))
+        else:
+            continue
+
+print (cube)
