17712a260f40b154ab2dc71219a7aca5d8d03175
Sebastian <seblowes@btinternet.com>
2022-07-24 12:23:25 +0100

    (refactor): Modify enum class to remove final eval

--- cube_functions.py
+++ cube_functions.py
@@ -1,55 +1,55 @@
 import numpy as np
 import enum
 
-class Face(enum.Enum):
-    up_face = 1
-    down_face = 2
-    back_face = 3
-    front_face = 4
-    left_face = 5
-    right_face = 6
-
-up_face = [[[],[2,0,2],[2,0,1],[2,0,0],[]],
+class Side(bytes, enum.Enum):
+    def __new__(cls, value, face):
+        obj = bytes.__new__(cls, [value])
+        obj._value_ = value
+        obj.face = face
+        return obj
+        
+    up_face = (1, [[[],[2,0,2],[2,0,1],[2,0,0],[]],
             [[4,0,0],[0,0,0],[0,0,1],[0,0,2],[5,0,2]],
             [[4,0,1],[0,1,0],[0,1,1],[0,1,2],[5,0,1]],
             [[4,0,2],[0,2,0],[0,2,1],[0,2,2],[5,0,0]],
-            [[],[3,0,0],[3,0,1],[3,0,2],[]]]
+            [[],[3,0,0],[3,0,1],[3,0,2],[]]])
 
-down_face = [[[],[3,2,0],[3,2,1],[3,2,2],[]],
+    down_face = (2, [[[],[3,2,0],[3,2,1],[3,2,2],[]],
             [[4,2,2],[1,0,0],[1,0,1],[1,0,2],[5,2,0]],
             [[4,2,1],[1,1,0],[1,1,1],[1,1,2],[5,2,1]],
             [[4,2,0],[1,2,0],[1,2,1],[1,2,2],[5,2,2]],
-            [[],[2,2,1],[2,2,1],[2,2,0],[]]]
+            [[],[2,2,2],[2,2,1],[2,2,0],[]]])
 
-back_face = [[[],[0,0,2],[0,0,1],[0,0,0],[]],
+    back_face = (3, [[[],[0,0,2],[0,0,1],[0,0,0],[]],
             [[5,0,2],[2,0,0],[2,0,1],[2,0,2],[4,0,0]],
             [[5,1,2],[2,1,0],[2,1,1],[2,1,2],[4,1,0]],
             [[5,2,2],[2,2,0],[2,2,1],[2,2,2],[4,2,0]],
-                [[],[1,2,2],[1,2,1],[1,2,0],[]]]
+            [[],[1,2,2],[1,2,1],[1,2,0],[]]])
 
-front_face = [[[],[0,2,0],[0,2,1],[0,2,2],[]],
+    front_face = (4, [[[],[0,2,0],[0,2,1],[0,2,2],[]],
             [[4,0,2],[3,0,0],[3,0,1],[3,0,2],[5,0,0]],
             [[4,1,2],[3,1,0],[3,1,1],[3,1,2],[5,1,0]],
             [[4,2,2],[3,2,0],[3,2,1],[3,2,2],[5,2,0]],
-            [[],[1,0,0],[1,0,1],[1,0,2],[]]]
+            [[],[1,0,0],[1,0,1],[1,0,2],[]]])
 
-left_face = [[[],[0,0,0],[0,1,0],[0,2,0],[]],
+    left_face = (5, [[[],[0,0,0],[0,1,0],[0,2,0],[]],
             [[2,0,2],[4,0,0],[4,0,1],[4,0,2],[3,0,0]],
             [[2,1,2],[4,1,0],[4,1,1],[4,1,2],[3,1,0]],
             [[2,2,2],[4,2,0],[4,2,1],[4,2,2],[3,2,0]],
-            [[],[1,2,0],[1,1,0],[1,0,0],[]]]
+            [[],[1,2,0],[1,1,0],[1,0,0],[]]])
 
-right_face = [[[],[0,2,2],[0,1,2],[0,0,2],[]],
+    right_face = (6, [[[],[0,2,2],[0,1,2],[0,0,2],[]],
             [[3,0,2],[5,0,0],[5,0,1],[5,0,2],[2,0,0]],
             [[3,1,2],[5,1,0],[5,1,1],[5,1,2],[2,1,0]],
             [[3,2,2],[5,2,0],[5,2,1],[5,2,2],[2,2,0]],
-            [[],[1,0,2],[1,1,2],[1,2,2],[]]]
+            [[],[1,0,2],[1,1,2],[1,2,2],[]]])
+
 
 def create_cube():
     '''
     side_notation = [0, 1, 2, 3, 4, 5]
     '''
-    cube = np.empty(54, dtype=str).reshape(6,3,3)
+    cube = np.empty((6,3,3), dtype=str)
     side_notation = ['W', 'Y', 'B', 'G', 'O', 'R']
     for side in range(len(cube)):
         for row in range(len(cube[side])):
@@ -60,30 +60,32 @@ def create_cube():
 
 def rotate_side(cube, side, rotation):
     '''
+    Side(side).face[siderow][sidecolumn]
     '''
-    current_cube_side = np.empty(25, dtype=str).reshape(5,5)
-    for row in range(len(current_cube_side)):
-        for column in range(len(current_cube_side[row])):
-            index = eval(Face(side).name)[row][column]
+    rotating_side = np.empty((5,5), dtype=str)
+    for row in range(len(rotating_side)):
+        for column in range(len(rotating_side[row])):
+            index = Side(side).face[row][column]
             if index:
-                current_cube_side[row][column] = cube[index[0]][index[1]][index[2]]
+                rotating_side[row][column] = cube[index[0]][index[1]][index[2]]
             else:
-                current_cube_side[row][column] = ''
+                rotating_side[row][column] = ''
 
     if rotation == 1:
-        current_cube_side = np.rot90(current_cube_side, k=3)
+        rotating_side = np.rot90(rotating_side, k=3)
     else:
-        current_cube_side = np.rot90(current_cube_side, k=1)
+        rotating_side = np.rot90(rotating_side, k=1)
 
-    for row in range(len(current_cube_side)):
-        for column in range(len(current_cube_side[row])):
-            index = eval(Face(side).name)[row][column]
+    for row in range(len(rotating_side)):
+        for column in range(len(rotating_side[row])):
+            index = Side(side).face[row][column]
             if index:
-                cube[index[0]][index[1]][index[2]] = current_cube_side[row][column]
+                cube[index[0]][index[1]][index[2]] = rotating_side[row][column]
             else:
                 continue
     return cube
 
 
     def exploded_view(cube):
+
         return
--- test.py
+++ test.py
@@ -8,3 +8,28 @@ for idx in range(len(rotations)):
     new_cube = cube_functions.rotate_side(new_cube, rotations[idx][0], rotations[idx][1])
 
 print(new_cube)
+
+cube_explode = ''
+
+for row in range(len(new_cube[0])):
+    cube_explode = cube_explode + '   '
+    for column in range(len(new_cube[0][row])):
+        cube_explode += new_cube[0][row][column]
+    cube_explode = cube_explode + '      \n'
+
+middle = [4,3,5,2]
+
+for row in range(len(new_cube[0])):
+    for i in range(len(middle)):
+        for column in range(len(new_cube[middle[i]][row])):
+            cube_explode += new_cube[middle[i]][row][column]
+
+    cube_explode += '\n'
+
+for row in range(len(new_cube[1])):
+    cube_explode = cube_explode + '   '
+    for column in range(len(new_cube[1][row])):
+        cube_explode += new_cube[1][row][column]
+    cube_explode = cube_explode + '      \n'
+
+print(cube_explode)
