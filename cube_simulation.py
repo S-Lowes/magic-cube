18f12a4af1ce70a081ca35d326e2906a080bfaaf
Sebastian <seblowes@btinternet.com>
2022-07-24 17:51:49 +0100

    (refactor,feat): Added additional tests and met pep8 standards

--- cube_simulation.py
+++ cube_simulation.py
@@ -13,7 +13,8 @@ print('Cube Created\nTime to Rotate')
 rotate = 'y'
 
 while rotate.startswith('y'):
-    print('What side would you like to rotate?\n1) Up\n2) Down\n3) Back\n4) Front\n5) Left\n6) Right')
+    print('What side would you like to rotate?' +
+          '\n1) Up\n2) Down\n3) Back\n4) Front\n5) Left\n6) Right')
     while True:
         try:
             r_side = int(input('Enter Side Number: '))
@@ -22,7 +23,7 @@ while rotate.startswith('y'):
                 break
             else:
                 print('Options between 1-6')
-        except:
+        except ValueError:
             print('That is not a valid option!')
 
     print('Which direction Of Rotation?\n1) Clockwise\n2) Anti-Clockwise')
@@ -34,10 +35,10 @@ while rotate.startswith('y'):
                 break
             else:
                 print('1 For Clockwise or 2 For Anti-Clockwise')
-        except:
+        except ValueError:
             print('That is not a valid option!')
-    
-    new_cube = cube_functions.rotate_side(new_cube,r_side, r_dir)
+
+    new_cube = cube_functions.rotate_side(new_cube, r_side, r_dir)
 
     rotate = input('Do you want to make another rotation? Yes/No \n').lower()
 
--- cube_test.py
+++ cube_test.py
@@ -1,14 +1,37 @@
 import cube_functions
 
-new_cube = cube_functions.create_cube()
-rotations = [[4,1],[6,2],[1,1],[3,2],[5,1],[2,2]]
+solved_cube = cube_functions.create_cube()
+test_cube = cube_functions.create_cube()
+rotations = [[4, 1], [6, 2], [1, 1], [3, 2], [5, 1], [2, 2]]
 
 for idx in range(len(rotations)):
-    new_cube = cube_functions.rotate_side(new_cube, rotations[idx][0], rotations[idx][1])
-print(cube_functions.exploded_view(new_cube))
+    test_cube = cube_functions.rotate_side(
+        test_cube, rotations[idx][0], rotations[idx][1])
 
-opposite_rotations = [[2,1],[5,2],[3,1],[1,2],[6,1],[4,2]]
+rotated_cube = cube_functions.exploded_view(test_cube)
+expected_cube = open('magic-cube/expected_cube.txt', 'r')
+
+if expected_cube.read() != rotated_cube:
+    raise Exception("Rotation operation flawed")
+
+print(rotated_cube)
+
+opposite_rotations = [[2, 1], [5, 2], [3, 1], [1, 2], [6, 1], [4, 2]]
 
 for idx in range(len(opposite_rotations)):
-    new_cube = cube_functions.rotate_side(new_cube, opposite_rotations[idx][0], opposite_rotations[idx][1])
-print(cube_functions.exploded_view(new_cube))
+    test_cube = cube_functions.rotate_side(
+        test_cube, opposite_rotations[idx][0], opposite_rotations[idx][1])
+
+test_condition = True
+while test_condition:
+    if len(solved_cube) == len(test_cube):
+        for side in range(len(test_cube)):
+            for row in range(len(test_cube[side])):
+                for column in range(len(test_cube[side][row])):
+                    if solved_cube[side][row][column] != \
+                         test_cube[side][row][column]:
+                        raise Exception("Cubes Elements not identical")
+        test_condition = False
+    else:
+        raise Exception("Cubes sizes miss matched")
+print(cube_functions.exploded_view(test_cube))
